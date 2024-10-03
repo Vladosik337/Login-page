c2aef4c1b50a1d14adca33b780ded9dc0cd16a65
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-10-04 00:49:36 +0300

    [FIX] column

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import './styles/style.scss'
 import Input from "./components/input.tsx";
 import Button from "./components/button.tsx";
-import MyButton from "./components/UI/button/MyButton.tsx";
-import MyInput from "./components/UI/input/MyInput.tsx";
-import classes from "./components/UI/button/MyButton.module.css";
 
 
 function App() {
@@ -12,10 +9,10 @@ function App() {
         <div className="min-h-full flex flex-col items-center justify-center overflow-clip h-screen">
             <div className="flex flex-col items-center gap-[20px]">
                 <img src="src/assets/card.svg" alt="" className="mb-[51px]"/>
-                <MyInput input={{id: 'user-name',placeholder: 'UserName', imgSrc: './src/assets/User.svg'}}/>
-                <MyInput input={{id: 'user-password',placeholder: 'UserPassword', imgSrc: './src/assets/lock.svg'}}/>
-                <MyButton customClass={`font-semibold text-blue  mt-[23px] hover:text-white hover:bg-blue hover:shadow-button w-full bg-white shadow-button rounded-[4px] h-[45px] flex items-center justify-center transition duration-300`}>Login</MyButton>
-                <MyButton customClass={`font-medium text-white -mt-[9px] hover:underline w-full h-[20px] text-right`}>Forgot password?</MyButton>
+                <Input input={{id: 'user-name',placeholder: 'UserName', imgSrc: './src/assets/User.svg'}}/>
+                <Input input={{id: 'user-password',placeholder: 'UserPassword', imgSrc: './src/assets/lock.svg'}}/>
+                <Button customClass={`font-semibold text-blue  mt-[23px] hover:text-white hover:bg-blue hover:shadow-button w-full bg-white shadow-button rounded-[4px] h-[45px] flex items-center justify-center transition duration-300`}>Login</Button>
+                <Button customClass={`font-medium text-white -mt-[9px] hover:underline w-full h-[20px] text-right`}>Forgot password?</Button>
             </div>
         </div>
     )
